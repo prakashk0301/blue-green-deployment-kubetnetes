60886da0e88c3029de32478652e8a6d63708bd6e
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2020-07-25 03:17:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    return "Hi, This code is develop by Prakash Kumar: 2", 200
+    return "Hi, This code is develop by Prakash Kumar: 1", 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
